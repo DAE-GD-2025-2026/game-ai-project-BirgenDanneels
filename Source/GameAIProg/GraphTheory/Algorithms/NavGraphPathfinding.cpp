d37f5641f6f24a060271b5ebe6bfe03ad57a827e
BirgenDanneels <birgen.danneels@student.howest.be>
2026-04-04 22:48:26 +0200

    Implemented NavMeshPathfinding::FindPath
    - Implemented NavMeshPathfinding::FindPath

--- Source/GameAIProg/GraphTheory/Algorithms/NavGraphPathfinding.cpp
+++ Source/GameAIProg/GraphTheory/Algorithms/NavGraphPathfinding.cpp
@@ -15,19 +15,80 @@ std::vector<FVector2D> NavMeshPathfinding::FindPath(const FVector2D& startPos, c
 	std::vector<FVector2D> finalPath{};
 
 	//Get the start and endTriangle
-
+	const auto* startTriangle{ pNavGraph->GetNavPolygon()->GetTriangleAtPosition(startPos, true) };
+	const auto* endTriangle{ pNavGraph->GetNavPolygon()->GetTriangleAtPosition(endPos, true) };
+	
 	//We have valid start/end triangles and they are not the same
+	if (!startTriangle || !endTriangle)
+	{
+		debugNodePositions = finalPath;
+		return finalPath;
+	}
+	
+	if (startTriangle == endTriangle)
+	{
+		finalPath.push_back(startPos);
+		finalPath.push_back(endPos);
+		debugNodePositions = finalPath;
+
+		return finalPath;
+	}
+	
 	//=> Start looking for a path
 	//Copy the graph
+	auto pNavGraphClone = pNavGraph->Clone();
 
 	//Create Extra node for the Start Node (Agent's position
-
+	auto startNode = std::make_unique<NavGraphNode>(startPos, -1);
+	const int startNodeId = pNavGraphClone->AddNode(std::move(startNode));
+	
+	for (const auto& edge : startTriangle->GetEdges())
+	{
+		std::optional<int> edgeIdx = pNavGraph->GetNavPolygon()->FindEdgeIndex(edge);
+		if (!edgeIdx.has_value()) continue;
+		
+		int nodeId = pNavGraphClone->GetNodeIdFromEdgeIndex(*edgeIdx);
+		if (nodeId == Graphs::InvalidNodeId) continue;
+			
+		pNavGraphClone->AddConnection(startNodeId, nodeId);
+	}
+	
 	//Create extra node for the endNode
+	auto endNode = std::make_unique<NavGraphNode>(endPos, -1);
+	int const endNodeId = pNavGraphClone->AddNode(std::move(endNode));
 
+	for (auto const& edge : endTriangle->GetEdges())
+	{
+		std::optional<int> edgeIdx = pNavGraph->GetNavPolygon()->FindEdgeIndex(edge);
+		if (!edgeIdx.has_value()) continue;
+		
+		int nodeId = pNavGraphClone->GetNodeIdFromEdgeIndex(*edgeIdx);
+		if (nodeId == Graphs::InvalidNodeId) continue;
+		
+		pNavGraphClone->AddConnection(endNodeId, nodeId);
+	}
+	
+	pNavGraphClone->SetConnectionCostsToDistances();
+	
 	//Run A star on new graph
+	AStar AStar(pNavGraphClone.get(), HeuristicFunctions::Euclidean);
 
+	auto nodePath = AStar.FindPath(
+		pNavGraphClone->GetNode(startNodeId).get(),
+		pNavGraphClone->GetNode(endNodeId).get()
+	);
+	
+	finalPath.reserve(nodePath.size());
+	for (const auto pPathNode : nodePath)
+	{
+		const auto& nodePosition = pPathNode->GetPosition();
+
+		finalPath.emplace_back(nodePosition);
+	}
+	
 	//Debug Visualisation
-
+	debugNodePositions = finalPath;
+	
 	// Extra: Run optimiser on new graph (First check if everything works without SSFA!)
 	// debugPortals = SSFA::FindPortals(nodes, *pNavGraph->GetNavPolygon());
 	// finalPath = SSFA::OptimizePortals(debugPortals, *pNavGraph->GetNavPolygon());
