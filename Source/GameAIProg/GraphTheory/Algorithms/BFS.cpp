87fe1ea45466361d7204b8e2032d9ecd9e8be40c
BirgenDanneels <birgen.danneels@student.howest.be>
2026-04-04 19:42:37 +0200

    Add BFS pathfinding and increase mud cost
    - Implemented BFS::FindPath
    - Adjusted TerrainGridGraph mud cost multiplier from 2.0f to 10.0f.

--- Source/GameAIProg/GraphTheory/Algorithms/BFS.cpp
+++ Source/GameAIProg/GraphTheory/Algorithms/BFS.cpp
@@ -2,6 +2,8 @@
 
 #include <map>
 #include <queue>
+#include <set>
+
 
 #include "Shared/Graph/Graph.h"
 
@@ -16,5 +18,48 @@ BFS::BFS(Graph* const pGraph)
 std::vector<Node*> BFS::FindPath(Node* const pStartNode, Node* const pDestinationNode) const
 {
 	std::vector<Node*> path;
+	
+	std::queue<Node*> openList;
+	std::set<Node*> visited;
+	std::map<Node*, Node*> parent;
+	
+	openList.push(pStartNode);
+	visited.insert(pStartNode);
+	
+	while(!openList.empty())
+	{
+		Node* currentNode = openList.front();
+		openList.pop();
+		
+		if (currentNode == pDestinationNode)
+		{
+			//Return Reconstruct path
+			currentNode = pDestinationNode;
+			
+			while (currentNode != pStartNode)
+			{
+				path.push_back(currentNode);
+				currentNode = parent[currentNode];
+			}
+			
+			path.push_back(pStartNode);
+			std::reverse(path.begin(), path.end());
+			
+			return path;
+		}
+		
+		for (auto connection : pGraph->FindConnectionsFrom(currentNode->GetId()))
+		{
+			Node* neighbor = pGraph->GetNode(connection->GetToId()).get();
+			
+			if (visited.find(neighbor) == visited.end())
+			{
+				visited.insert(neighbor);
+				parent[neighbor] = currentNode;
+				openList.push(neighbor);
+			}
+		}
+	}
+	
 	return path;
 }
